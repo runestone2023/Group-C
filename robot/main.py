a3d37ccec0e2801226dcf1fbd079127e40d1c0d4
Kax-y <56028244+Kax-y@users.noreply.github.com>
2023-04-27 11:29:34 +0200

    Refactor robot files & added action handling

--- robot/main.py
+++ robot/main.py
@@ -1,39 +1,22 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-import socket
+from src.event import EventSource
+from src.movement import move, beep
+from src.patrol import patrol
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+import uasyncio
 
+def main():
+    es = EventSource('127.0.0.1', '/api/v1/robot/hello', 8080)
+    es.add_event_listener('Beep', beep)
+    es.add_event_listener('Move', move)
+    es.add_event_listener('Patrol', patrol)
 
-# Create your objects here.
-ev3 = EV3Brick()
+    loop = uasyncio.get_event_loop()
+    loop.create_task()
+    loop.run_forever()
 
-def micropython_socket_test(url, port):
-    s = socket.socket()
-
-    ai = socket.getaddrinfo(url, port)
-    # print("Address infos:", ai)
-    addr = ai[0][-1]
-
-    # print("Connect address:", addr)
-    s.connect(addr)
-
-    s.send(b"GET /api/v1/robot/hello HTTP/1.0\r\n\r\n")
-    print(s.recv(4096))
-
-    s.close()
 
 # Write your program here.
 if __name__ == '__main__':
-    print("bongus")
-    micropython_socket_test("example.com", 80)
-
-
-
+    print("Starting robot...")
+    main()
+    
--- robot/src/stream.py
+++ robot/src/stream.py
@@ -1,12 +1,15 @@
 import asyncio
 import re
-import socket
+import json
 
 EVENT_REGEX = re.compile('event:(.*)\n')
+
 # NOTE: This is not in line with specification, as data can contain newlines.
 DATA_REGEX = re.compile('data:(.*)\n')
+
 ID_REGEX = re.compile('id:([0-9])\n')
 RETRY_REGEX = re.compile('retry:([0-9]*)\n')
+
 
 class Event:
     def __init__(self, event='message', data='', id=None, retry=None):
@@ -17,10 +20,12 @@ class Event:
 
     def parse_from_string(self, input_string):
         self.event = re.search(EVENT_REGEX, input_string).group(0)
-        self.data = re.search(DATA_REGEX, input_string).group(0)
+
+        data = re.search(DATA_REGEX, input_string).group(0)
+        self.data = None if data is None else json.loads(data)
+
         self.id = re.search(ID_REGEX, input_string).group(0)
         self.retry = re.search(RETRY_REGEX, input_string).group(0)
-
 
 
 class EventSource:
@@ -37,37 +42,22 @@ class EventSource:
     def __anext__(self):
         pass
 
-def micropython_socket_test(url, port):
-    s = socket.socket()
-
-    ai = socket.getaddrinfo(url, port)
-    # print("Address infos:", ai)
-    addr = ai[0][-1]
-
-    # print("Connect address:", addr)
-    s.bind(('0.0.0.0', 8000))
-    s.connect(addr)
-
-    # endpoint for API: /api/v1/robot/hello
-    s.send(b"GET /api/v1/robot/hellostream HTTP/1.0\r\n\r\n")
-    while True:
-        print(s.recv(4096))
-
-    s.close()
 
 # No async iteration, try another approach.
 async def fetch_commands(endpoint_url: str, port=80):
     reader, writer = await asyncio.open_connection(endpoint_url, port)
     writer.write(b"GET /api/v1/robot/hellostream HTTP/1.0\r\n\r\n")
+
     while True:
         yield reader.read(4096)
 
 
 async def main():
     async for command in fetch_commands("localhost", 8080):
-        print(await command)
-
+        cmd = await command
+        print(cmd)
+        
 
 if __name__ == '__main__':
     asyncio.run(main())
-    #micropython_socket_test("127.0.0.1",8080)
+    #micropython_socket_test("127.0.0.1", 8080)
