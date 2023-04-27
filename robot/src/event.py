0d08502e69068be09a107407f3eb20777a77140f
Oskar Pettersson Löfstedt <oskar.petterssonlofstedt.4134@student.uu.se>
2023-04-27 16:07:41 +0200

    Functional eventsource and connection tests, as of yet not compatible with the robot.
    Co-authored-by: Kax-y <Kax-y@users.noreply.github.com>

--- robot/src/event.py
+++ robot/src/event.py
@@ -3,13 +3,13 @@ import re
 import json
 
 
-EVENT_REGEX = re.compile('event:(.*)\n')
+EVENT_REGEX = re.compile(r'event:(.*?)\n')
 
 # NOTE: This is not in line with specification, as data can contain newlines.
-DATA_REGEX = re.compile('data:(.*)\n')
+DATA_REGEX = re.compile(r'data:(.*?)\n')
 
-ID_REGEX = re.compile('id:([0-9])\n')
-RETRY_REGEX = re.compile('retry:([0-9]*)\n')
+ID_REGEX = re.compile(r'id:([0-9]*?)\n')
+RETRY_REGEX = re.compile(r'retry:([0-9]*?)\n')
 
 
 class Event:
@@ -18,6 +18,9 @@ class Event:
         self.data = data
         self.id = id
         self.retry = retry
+    
+    def __repr__(self):
+        return "{}({}, {}, {}, {})".format(self.__class__.__name__, self.event, self.data, self.id, self.retry)
 
     @classmethod
     def parse_from_string(cls, input_string):
@@ -31,7 +34,7 @@ class Event:
     @staticmethod
     def check_regex(regex, input_string, default):
         match = re.search(regex, input_string)
-        return match if match is not None else default
+        return match.group(1) if match is not None else default
 
 
 class EventSource:
@@ -43,13 +46,14 @@ class EventSource:
         self.dispatch_dict = {}
         self.READ_SIZE = read_size
         print("Initialized event source")
-        #asyncio.create_task(self.event_loop())
+        
+        asyncio.get_event_loop().create_task(self.event_loop())
 
     def add_event_listener(self, event_name, handler_function):
         self.dispatch_dict[event_name] = handler_function
 
 
-    async def dispatch(self, event):
+    def dispatch(self, event):
         action = self.dispatch_dict.get(event.event, id)
         action(event)
 
@@ -67,4 +71,5 @@ class EventSource:
             
             if buf != b'' and not buf.startswith(b':'):
                 event = Event.parse_from_string(buf.decode('utf-8'))
-                await self.dispatch(event)
+                print(event)
+                self.dispatch(event)
