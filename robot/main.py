a3305be828fa28905842edd5e7cfa8a536ef9aa2
Leon Philip <leon.hejdenberg@gmail.com>
2023-05-01 22:58:54 +0200

    Created functions for robot control and moved into separate module

--- robot/main.py
+++ robot/main.py
@@ -6,38 +6,18 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import control
 
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
+if __name__ == "__main__":
+    # Create object for EV3 and its sensors
+    ev3 = EV3Brick()
+    left_motor = Motor(Port.A)
+    right_motor = Motor(Port.B)
+    obstacle_sensor = UltrasonicSensor(Port.S4)
 
+    # Create object for the drivebase
+    robot = DriveBase(left_motor, right_motor, wheel_diameter=50, axle_track=90)
 
-# Create your objects here.
-ev3 = EV3Brick()
-
-
-# Write your program here.
-#ev3.speaker.beep()
-left_motor = Motor(Port.A)
-right_motor = Motor(Port.B)
-obstacle_sensor = UltrasonicSensor(Port.S4)
-
-#motor.run_time(360, 10000, then=Stop.HOLD, wait=True)
-
-robot = DriveBase(left_motor, right_motor, wheel_diameter=50, axle_track=90)
-
-while True:
-    # Begin driving forward at 200 millimeters per second.
-    robot.drive(200, 0)
-
-    # Wait until an obstacle is detected. This is done by repeatedly
-    # doing nothing (waiting for 10 milliseconds) while the measured
-    # distance is still greater than 300 mm.
-    while obstacle_sensor.distance() > 300:
-        wait(10)
-
-    # Drive backward for 300 millimeters.
-    robot.straight(-300)
-
-    # Turn around by 120 degrees
-    robot.turn(120)
+    # Start automatic mode
+    test_automatic_movement(robot, obstacle sensor)
