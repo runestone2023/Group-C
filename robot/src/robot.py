6d7260ef026a34711109f5c10906905c99dd2c1b
Leon Philip <leon.hejdenberg@gmail.com>
2023-05-08 16:41:01 +0200

    Updated routes to use events

--- robot/src/robot.py
+++ robot/src/robot.py
@@ -2,13 +2,14 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
+from event import Event, EventSource # EventSource import needed?
 
 import uasyncio
 
 
 
 class Robot:
-    def __init__(self):
+    def __init__(self, event_source):
         self.ev3 = EV3Brick()
 
         # Create object for the drivebase
@@ -19,25 +20,28 @@ class Robot:
 
         self.ultrasonic_sensor = UltrasonicSensor(Port.S4)
 
-        self.routes = {1: [("forward", 300), ("rotate", 90), ("forward", 300), ("rotate", 90),
-                      ("forward", 300), ("rotate", 90), ("forward", 300), ("rotate", 90)]}
+        self.routes = {1: [Event("MoveDistance", '{argument: 300}'), Event("Rotate", '{argument: 90}'),
+                           Event("MoveDistance", '{argument: 300}'), Event("Rotate", '{argument: 90}',
+                           Event("MoveDistance", '{argument: 300}'), Event("Rotate", '{argument: 90}'))]}
+
+        self.event_source = event_source
 
 
-    def set_movement_speed(self, speed):
+    async def set_movement_speed(self, speed):
         self.drive_base.settings(speed=speed)
 
     # Get the distance traveled by the robot in centimeters
-    def distance_travelled(self):
+    async def distance_travelled(self):
         return self.drive_base.distance() / 10
 
-    def obstacle_in_front(self):
+    async def obstacle_in_front(self):
         return self.ultrasonic_sensor.distance() > 300
 
-    def stop_moving(self):
+    async def stop_moving(self):
         self.drive_base.stop()
 
     # Direction == 1 is forward, direction == -1 is backward
-    def move(self, direction):
+    async def move(self, direction):
         if direction == 1:
             self.drive_base.drive(200, 0)
         elif direction == -1:
@@ -46,20 +50,17 @@ class Robot:
             self.stop_moving()
 
     # Positive distance is forward, negative distance is backward
-    def move_distance(self, distance):
+    async def move_distance(self, distance):
         self.drive_base.straight(distance)
 
     # Positive angle is clockwise, negative angle is counterclockwise
-    def rotate(self, angle):
+    async def rotate(self, angle):
         self.drive_base.turn(angle)
 
     async def follow_route(self, route_id):
         route = self.routes.get(route_id)
-        for command in route:
-            if command[0] == "forward":
-                self.drive_base.straight(command[1])
-            elif command[0] == "rotate":
-                self.drive_base.turn(command[1])
+        for event in route:
+            self.event_source.dispatch_event(event)
 
     async def patrol(self, _):
         while True:
